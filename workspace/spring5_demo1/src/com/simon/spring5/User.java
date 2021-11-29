430414ae3891364faa3c2c6538032d115a469484
xuanhsh <xuanhsh@inspur.com>
2021-11-29 20:15:14 +0800

    第一次提交

---
+++ workspace/spring5_demo1/src/com/simon/spring5/User.java
@@ -0,0 +1,11 @@
+package com.simon.spring5;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import javax.xml.soap.SOAPPart;
+
+public class User {
+    public void add(){
+        System.out.println("add.....");
+    }
+}
