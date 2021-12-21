9bf39c31ee68c7ccb34bc0015c370846de33b2c1
xuanhsh <xuanhsh@inspur.com>
2021-12-21 09:13:39 +0800

    20211213-2:Bean管理-XML

---
+++ workspace/spring5_demo2/src/com/simon/spring5/factorybean/MyBean.java
@@ -0,0 +1,25 @@
+package com.simon.spring5.factorybean;
+
+import com.simon.spring5.collectiontype.Course;
+import org.springframework.beans.factory.FactoryBean;
+
+public class MyBean implements FactoryBean<Course> {
+
+    // 定义返回bean
+    @Override
+    public Course getObject() throws Exception {
+        Course course = new Course();
+        course.setCname("abd");
+        return course;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return FactoryBean.super.isSingleton();
+    }
+}
--- workspace/spring5_demo2/src/com/simon/spring5/testdemo/TestSpring5Demo1.java
+++ workspace/spring5_demo2/src/com/simon/spring5/testdemo/TestSpring5Demo1.java
@@ -1,7 +1,9 @@
 package com.simon.spring5.testdemo;
 
 import com.simon.spring5.collectiontype.Book;
+import com.simon.spring5.collectiontype.Course;
 import com.simon.spring5.collectiontype.Stu;
+import com.simon.spring5.factorybean.MyBean;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,15 +11,31 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring5Demo1 {
 
     @Test
-    public void testCollection(){
+    public void test2(){
         // 1.加载spring配置文件
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean2.xml");
 
         // 2.获取配置创建的对象
-        Book book = context.getBean("book", Book.class);
+        Book book1 = context.getBean("book", Book.class);
+        Book book2 = context.getBean("book", Book.class);
+//IOC 操作Bean管理(bean作用域)。
+//1、在 Spring里面,设置创建bean实例是单实例还是多实例
+//默认是单实例duixiang
+        System.out.println(book1);
+        System.out.println(book2);
 
-        book.test();
+    }
+    @Test
+    public void testC3(){
+        // 1.加载spring配置文件
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean3.xml");
+
+        // 2.获取配置创建的对象
+        Course course = context.getBean("myBean", Course.class);
+
+        System.out.println(course);
 
     }
 
